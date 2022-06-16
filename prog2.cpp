ea8c8d44ddd0be32b17ca7c7a12089f720478b70
YajneshVishwakarma <75421738+YajneshVishwakarma@users.noreply.github.com>
2022-06-16 10:27:03 +0530

    Update prog2.cpp

--- prog2.cpp
+++ prog2.cpp
@@ -56,6 +56,73 @@ class Student
             }
 
         }
+
+        bool search()
+        {
+            int flag = 0;
+            char filename[20];
+            cout<<"Enter the filename\n";
+            cin>>filename;
+
+            fstream file;
+            file.open(filename,ios::in);
+
+            char key[100];
+            cout<<"Enter the keyword\n";
+            cin>>key;
+            char extra[45];
+
+            while(!file.eof())
+            {
+                file.getline(name,20,'|'); 
+                file.getline(sem,10,'|');
+                file.getline(branch,20,'|');
+                file.getline(extra,45,'\n');
+
+                if (strcmp(key,name) == 0)
+                {
+                    cout<<name<<" | "<<sem<<" | "<<branch<<" | "<<extra<<endl;
+                    flag  = 1;
+                }
+            }
+            if (flag == 1)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        void modify()
+        {
+            char filename[20];
+            cout<<"Enter the filename\n";
+            cin>>filename;
+
+            fstream file;
+            file.open(filename,ios::in);
+
+
+            char key[100];
+            cout<<"Enter the keyword to change\n";
+            cin>>key;
+            char extra[45];
+
+            while(!file.eof())
+            {
+                file.getline(name,20,'|'); 
+                file.getline(sem,10,'|');
+                file.getline(branch,20,'|');
+                file.getline(extra,45,'\n');
+
+                if (strcmp(key,name) == 0)
+                {
+                    cout<<name<<" | "<<sem<<" | "<<branch<<" | "<<extra<<endl;
+                    return;
+                }
+            }
+
+        }
     
 };
 
@@ -69,8 +136,17 @@ int main()
     do
     {
         
-        cout<<"Enter your choice\n1.Pack Student Object\n2.Unpack Student Object\n3.Exit\n";
+        cout<<"Enter your choice\n1.Pack Student Object\n2.Unpack Student Object\n3.Search a Keyword\n4.Modify the data\n0 to Exit\n";
         cin>>exp;
+
+        if (exp == 0)
+        {
+            cout<<"\n**********************************************\n";
+            cout<<"Exiting the program\n";
+            cout<<"**********************************************\n";
+            break;
+        }
+        
             switch (exp)
         {
         case 1:
@@ -85,7 +161,7 @@ int main()
 
             for (int i = 0; i < num; i++)
             {
-                cout<<"**********************************************\n";
+                cout<<"\n**********************************************\n";
                 st.pack(ofile);
                 cout<<"**********************************************\n\n";
             }
@@ -102,11 +178,36 @@ int main()
             break;
             ifile.close();
 
+
+         case 3:
+
+            
+            if (st.search())
+            {
+                cout<<"**********************************************\n";
+                cout<<"Keyword found!!\n";
+                cout<<"**********************************************\n\n";
+            }
+            else
+            {
+                cout<<"**********************************************\n";
+                cout<<"Keyword not found!!\n";
+                cout<<"**********************************************\n\n";
+            }
+            
+            break;
+            ifile.close();
+
+        case 4:
+
+            st.modify();
+            break;
+
         default:
                 cout<<"Invalid choice";
             break;
         }
-    }while(exp!=3);
+    }while(1);
     
    
     return 0;
